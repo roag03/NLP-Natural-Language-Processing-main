1ccf01161ef2b7eb50a45ec46f0f825c3850e86d
roag03 <agraharidurgesh09@gmail.com>
2024-02-09 12:42:41 +0530

    Initial commit

---
+++ NLP-Natural-Language-Processing-main/zz_test_python.py
@@ -0,0 +1,30 @@
+#Printing something
+a= 3
+print(a)
+
+#Lists: making and operstaions
+
+food = ["Fries","Onion_rings", "chips"]
+drinks = ["Rum", "whiskey", "Vodka"]
+
+bar = food + drinks
+
+print(bar)
+
+drinks.append("Cocktails")
+food.insert(2,"Cheese_cubes")
+
+print(food,bar)
+
+drinks.append("coffee")
+print(food)
+
+bar = drinks+food
+
+print(bar)
+
+bar.append("Jin")
+
+print(bar)
+
+
